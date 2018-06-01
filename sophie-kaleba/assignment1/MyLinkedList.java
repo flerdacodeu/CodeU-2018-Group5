1f49e3c2115b0837614a46eb3d80666b4282d141
Sophie Kaleba <sophie.kaleba@gmail.com>
2018-06-02 00:17:18 +0200

    got rid of loop for index, fix exceptions, turn into static method

--- sophie-kaleba/assignment1/MyLinkedList.java
+++ sophie-kaleba/assignment1/MyLinkedList.java
@@ -1,27 +1,24 @@
 import java.util.LinkedList;
 
 /**
- * Question 2
- * Implement an algorithm to find the kth to last element of a singly linked list
- *
+ * find the kth to last element of a singly linked list
  */
 public class MyLinkedList {
-	
+
 	/**
 	 * find the kth to last element of a singly linked list
 	 * @param linkedList - the linked list we are going through
 	 * @param k - index regarding the last element of the list
 	 * @return - the kth to last element of the list
-	 * @throws Exception - if the parameter is out of bounds
+	 * @throws IndexOutOfBoundsException - if the parameter is out of bounds
+	 * @throws IllegalArgumentException - if the list is empty
 	 */
-	public int kThToLastElement(LinkedList<Integer> linkedList, int k) throws Exception {
+	public static int kThToLastElement(LinkedList<Integer> linkedList, int k) throws IndexOutOfBoundsException, IllegalArgumentException {
 		int size = linkedList.size();
-		if (size < k || k < 0 ) throw new Exception("k is not valid. It must be part of the interval [0;size of list[");
-		
-		int currentIndex = 0;
-		while (k+currentIndex != size-1) {
-			currentIndex++;			
-		}
+		if (linkedList.isEmpty()) throw new IllegalArgumentException("The provided list is empty");
+		if (size < k || k < 0 ) throw new IndexOutOfBoundsException(String.format("size=%s, index=%s", size, k));
+
+		int currentIndex = size - 1 - k;
 		return linkedList.get(currentIndex);
 	}
 
--- sophie-kaleba/assignment1/MyLinkedListTest.java
+++ sophie-kaleba/assignment1/MyLinkedListTest.java
@@ -6,13 +6,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class MyLinkedListTest {
-	
-	private MyLinkedList body;
+
 	private LinkedList<Integer> myList;
-	
+
 	@Before
 	public final void init(){
-		this.body = new MyLinkedList();
 		this.myList = new LinkedList<Integer>();
 		this.myList.add(1);
 		this.myList.addLast(2);
@@ -20,34 +18,40 @@ public class MyLinkedListTest {
 		this.myList.addLast(4);
 		this.myList.addLast(5);
 	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public final void testKthElementParameterTooBig() throws IndexOutOfBoundsException, IllegalArgumentException {
+		MyLinkedList.kThToLastElement(this.myList, 8);
+	}
 	
-    @Test(expected = Exception.class)
-    public final void testKthElementParameterTooBig() throws Exception {
-    	this.body.kThToLastElement(this.myList, 8);
-    }
-    
-    @Test(expected = Exception.class)
-    public final void testKthElementParameterTooSmall() throws Exception {
-    	this.body.kThToLastElement(this.myList, -1);
-    }
-    
-    @Test
-    public final void testKthElementNotZero() throws Exception {
-    	int value = this.body.kThToLastElement(this.myList, 1);
-    	assertTrue(value==4);
-    }
-    
-    @Test
-    public final void testKthElementZero() throws Exception {
-    	int value = this.body.kThToLastElement(this.myList, 0);
-    	assertTrue(value==5);
-    }
-    
-    @Test
-    public final void testKthElementMax() throws Exception {
-    	int value = this.body.kThToLastElement(this.myList, 4);
-    	assertTrue(value==1);
-    }
-   
+	@Test(expected = IllegalArgumentException.class)
+	public final void testKthElementEmptyList() throws IndexOutOfBoundsException, IllegalArgumentException {
+		this.myList = new LinkedList<Integer>();
+		MyLinkedList.kThToLastElement(this.myList, 1);
+	}
+
+	@Test(expected = Exception.class)
+	public final void testKthElementParameterTooSmall() throws IndexOutOfBoundsException,IllegalArgumentException {
+		MyLinkedList.kThToLastElement(this.myList, -1);
+	}
+
+	@Test
+	public final void testKthElementNotZero() throws IndexOutOfBoundsException, IllegalArgumentException {
+		int value = MyLinkedList.kThToLastElement(this.myList, 1);
+		assertTrue(value==4);
+	}
+
+	@Test
+	public final void testKthElementZero() throws IndexOutOfBoundsException, IllegalArgumentException {
+		int value = MyLinkedList.kThToLastElement(this.myList, 0);
+		assertTrue(value==5);
+	}
+
+	@Test
+	public final void testKthElementMax() throws IndexOutOfBoundsException, IllegalArgumentException {
+		int value = MyLinkedList.kThToLastElement(this.myList, 4);
+		assertTrue(value==1);
+	}
+
 
 }
