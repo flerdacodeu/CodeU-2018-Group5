b9795d1a056b591b6cd58cf3adaa4dffc4ef669f
Sophie Kaleba <sophie.kaleba@gmail.com>
2018-06-21 01:50:11 +0200

    add a bunch of tests. Algorithm still buggy, but almost there!

--- sophie-kaleba/assignment3/Grid.java
+++ sophie-kaleba/assignment3/Grid.java
@@ -4,12 +4,29 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
+/**
+ * The grid is not necessarily square
+ * The grid only contains single ascii characters
+ * The search is case insensitive
+ *
+ */
 public class Grid {
 
 	private List<String[]> content;
+	private List<int[]> markedContent;
+	private int width;
+	private String currentWord;
 
+	/**
+	 * build a grid out of a file
+	 * @param filename
+	 */
 	public Grid(String filename) {
 		try {
 			this.content = Grid.parseFile(filename);
@@ -17,6 +34,242 @@ public class Grid {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * build grid out of a string containing characters
+	 * TODO - assumption about the good width
+	 * TODO - add assertions
+	 * @param prefilledGrid - the string to be turned into a grid
+	 * @param wantedWidth - the wanted width of the grid
+	 */
+	public Grid(String prefilledGrid, int wantedWidth) {
+		this.markedContent = new ArrayList<int[]>();
+		this.width = wantedWidth;
+		
+		this.content = new ArrayList<String[]>();
+
+		String[] allNumbers = prefilledGrid.split(" ");
+		int gridSize = allNumbers.length;
+
+		if (gridSize >= wantedWidth) {
+			for (int i=0; i< gridSize ; i+=wantedWidth) {
+				int[] myArray = new int[wantedWidth];
+				this.markedContent.add(myArray);
+				this.content.add(Arrays.copyOfRange(allNumbers, i, i+wantedWidth));
+			}
+			this.resetMarkedContent();
+		}
+		else {
+			this.content.add(allNumbers);
+		}
+	}
+
+	/**
+	 * return the character at the given position in the grid
+	 * @param row - the row index in the grid
+	 * @param column - the column index in the grid
+	 * @return the character at this position in the grid
+	 * @throws IndexOutOfBoundsException
+	 */
+	String getCharAt(int row, int column) throws IndexOutOfBoundsException {
+		if ((0>row && row >this.content.size()) && (0>column && column>this.width)) throw new IndexOutOfBoundsException();
+		return this.content.get(row)[column];
+	}
+
+
+	/**
+	 * mark a character of the grid as visited
+	 * @param row - the row index in the marked grid
+	 * @param column - the column index in the marked grid
+	 */
+	void mark(int row, int column) {
+		this.markedContent.get(row)[column] = 1;
+	}
+	
+	
+	/**
+	 * unmark a character of the grid
+	 * @param row - the row index in the marked grid
+	 * @param column - the column index in the marked grid
+	 */
+	void unmark(int row, int column) {
+		this.markedContent.get(row)[column] = 0;
+	}
+
+
+	/**
+	 * check is character of the grid has already been visited
+	 * @param row - the row index in the marked grid
+	 * @param column - the column index in the marked grid
+	 * @return true if already visited, false otherwise
+	 */
+	boolean isMarked(int row, int column) {
+		return this.markedContent.get(row)[column] == 1;
+	}
+
+
+	/**
+	 * set all characters of this grid as non-visited
+	 */
+	void resetMarkedContent() {
+		for (int i = 0 ; i < this.markedContent.size() ; i ++) {
+			for (int j = 0 ; j < this.width ; j ++) {
+				this.markedContent.get(i)[j] = 0;
+			}
+		}
+	}
+
+	/**
+	 * Utter ugliness
+	 * for a given character, return the indexes of all reachable neighbours
+	 * @param row
+	 * @param column
+	 * @return the list of all reachable neighbours
+	 */
+	public List<int[]> allReachableNeighbours(int row, int column) {
+		
+		List<int[]> result = new ArrayList<int[]>();
+		
+		if (row - 1 >= 0) {
+			result.add(new int[]{row - 1 , column});
+			
+			if (column - 1 >= 0) {
+				result.add(new int[]{row - 1 , column - 1});
+			}
+			if ((column + 1 < this.width)) {
+				result.add(new int[]{row - 1 , column + 1});
+			}
+		}
+		
+		if (row + 1 < this.content.size()){
+			result.add(new int[]{row + 1 , column});
+			
+			if (column - 1 >= 0) {
+				result.add(new int[]{row + 1 , column - 1});
+			}
+			if ((column + 1 < this.width)) {
+				result.add(new int[]{row + 1 , column + 1});	
+			}
+		}
+		
+		if (column - 1 >= 0) {
+			result.add(new int[]{row, column - 1});
+		}
+		if (column + 1 < this.width) {
+			result.add(new int[]{row, column + 1});
+		}
+		
+		return result;
+	}
+	
+	
+	
+//	/**
+//	 * 
+//	 * @param dictionary
+//	 * @param row
+//	 * @param column
+//	 * @return
+//	 */
+//	public void wordSearchAux(List<String> dictionary, int row, int column, List<String> foundWords, int startRow, int startColumn) {
+//		this.mark(row, column);
+//		String potentialWord = this.currentWord+this.getCharAt(row, column);
+//		System.out.println(potentialWord);
+//		
+//		if (!Grid.isPrefix(dictionary, potentialWord)) {
+//			this.resetMarkedContent();
+//			this.mark(startRow, startColumn);
+//			return;
+//		}
+//		else if (Grid.isWord(dictionary, potentialWord)) {
+//			foundWords.add(potentialWord);
+//		}
+//		this.currentWord = potentialWord;
+//
+//		List<int[]> neigbours = this.allReachableNeighbours(row, column);
+//		for (int[] coordAux : neigbours) {
+//			int rowAux = coordAux[0];
+//			int columnAux = coordAux[1];
+//			
+//			if (!this.isMarked(rowAux, columnAux)) {
+//		
+//				this.wordSearchAux(dictionary, rowAux, columnAux, foundWords, startRow, startColumn);
+//			}
+//		}
+//	}
+	
+	public void wordSearchAux(List<String> dictionary, int row, int column, List<String> foundWords, int startRow, int startColumn) {
+
+		this.mark(row, column);
+		this.currentWord += this.getCharAt(row, column);
+		System.out.println(row+" "+column);
+		
+		List<int[]> neigbours = this.allReachableNeighbours(row, column);
+		for (int[] coordAux : neigbours) {
+			int rowAux = coordAux[0];
+			int columnAux = coordAux[1];
+			
+			if (!this.isMarked(rowAux, columnAux)) {
+				
+				String potentialWord = this.currentWord+this.getCharAt(rowAux, columnAux);
+				System.out.println(potentialWord);
+				if (Grid.isPrefix(dictionary, potentialWord)) {
+					if (Grid.isWord(dictionary, potentialWord)) {
+						foundWords.add(potentialWord);
+					}
+
+					this.wordSearchAux(dictionary, rowAux, columnAux, foundWords, startRow, startColumn);
+				}
+				else {
+					this.unmark(rowAux, columnAux);
+				}
+				
+			}
+		}
+	}
+	
+	
+	public static boolean isPrefix(List<String> dictionary, String word) {
+		for (String s : dictionary) {
+			if (s.startsWith(word)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public static boolean isWord(List<String> dictionary, String word) {
+		return dictionary.contains(word);
+	}
+	
+
+	/**
+	 * wanders through the grid to potentially find words from the dictionary
+	 * @param dictionary
+	 * @return a list if found words from the dictionary
+	 */
+	public List<List<String>> wordSearch(List<String> dictionary) {
+
+		List<List<String>> result = new ArrayList<List<String>>();
+		List<String> localResult = new ArrayList<String>();
+
+		for (int i = 0; i<this.content.size() ; i++) {
+			for (int j = 0; j < this.width ; j ++) {
+				System.out.println("START "+i+" "+j);
+				this.resetMarkedContent();
+				this.currentWord = ""; 
+				this.wordSearchAux(dictionary, i, j, localResult, i, j);
+				if (!localResult.isEmpty()) result.add(localResult);
+			}
+		}
+		return result;
+	}
+
+
+
+
+
 
 	/**
 	 * builds a grid filled with Characters out of a file
@@ -39,14 +292,14 @@ public class Grid {
 			if (line == null) throw new FileNotFoundException();
 			String[] arrayedLine = line.split(" ");
 			int gridWidth = arrayedLine.length;
-			
+
 			while ((line = b.readLine()) != null) {
 				arrayedLine = line.split(" ");
 				int currentWidth = arrayedLine.length;
 				if (currentWidth != gridWidth) throw new BadGridFileException("line width:"+currentWidth+", but must be: "+gridWidth);
 				finalGrid.add(arrayedLine);
 			}
-			
+
 			b.close();
 			return finalGrid;
 
@@ -55,5 +308,10 @@ public class Grid {
 			return finalGrid;
 		}
 
+	}
+	
+	
+	public List<String[]> getContent() {
+		return this.content;
 	}
 }
---
+++ sophie-kaleba/assignment3/GridTest.java
@@ -0,0 +1,77 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GridTest {
+	
+	private Grid testGrid;
+	private List<String> testDictionary;
+
+	@Before
+	public final void init() {
+		this.testGrid = new Grid("A A R T C D", 3);
+		this.testDictionary = Arrays.asList("CAR", "CARD", "CART", "CAT");
+	}
+	
+	@Test
+	public final void buildFromString() {
+		List<String[]> expectedContent = new ArrayList<String[]>();
+		expectedContent.add(new String[]{"A" , "A" , "R"});
+		expectedContent.add(new String[]{"T" , "C" , "D"});
+
+		assertEquals(expectedContent.size(), this.testGrid.getContent().size());
+		for (int i = 0; i < expectedContent.size() ; i ++) {
+			assertArrayEquals(expectedContent.get(i), this.testGrid.getContent().get(i));
+		}
+		
+	}
+	
+	
+//	@Test
+//	public final void wordSearchEmptyGrid(){
+//		
+//	}
+//	
+//	@Test
+//	public final void wordSearchEmptyDict() {
+//		
+//	}
+	@Test
+	public final void testGetCharAt() {
+		assertEquals("C", this.testGrid.getCharAt(1, 1));
+	}
+	
+	@Test
+	public final void testNeigbours() {
+		List<int[]> expectedResult = new ArrayList<int[]>();
+		expectedResult.add(new int[]{1 , 0});
+		expectedResult.add(new int[]{1 , 1});
+		expectedResult.add(new int[]{0 , 1});
+		int size = expectedResult.size();
+		
+		List<int[]> actualResult = this.testGrid.allReachableNeighbours(0, 0);
+		assertEquals(expectedResult.size(), actualResult.size());
+		for (int i = 0; i < size ; i++) {
+			assertArrayEquals(expectedResult.get(i), actualResult.get(i));
+			
+		}
+		
+	}
+	
+	@Test
+	public final void wordSearch3Words() {
+		List<List<String>> expectedResult = new ArrayList<List<String>>();
+		List<List<String>> result = this.testGrid.wordSearch(this.testDictionary);
+		for (int i=0 ; i < result.size() ; i++) {
+			for (int j = 0; j < result.get(i).size() ; j++) {
+				System.out.println(result.get(i).get(j));
+			}
+		}
+	}
+}
