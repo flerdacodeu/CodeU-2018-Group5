81891fa054564f1f3979a17ca42d8fb5f1f7cb9f
Sophie Kaleba <sophie.kaleba@gmail.com>
2018-07-03 23:58:27 +0200

    remove the unnecessary unique id

--- sophie-kaleba/assignment3/BadGridFileException.java
+++ sophie-kaleba/assignment3/BadGridFileException.java
@@ -1,7 +1,5 @@
 
 public class BadGridFileException extends Exception {
-	
-	private static final long serialVersionUID = 1L;
 
 	public BadGridFileException(String message) {
 		System.out.println(message);
