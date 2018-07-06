202758003794b54bd427b13462262a795d5580ef
andreeapetcu <38542030+andreeapetcu@users.noreply.github.com>
2018-07-06 14:55:56 +0100

    added additional tests

--- andreeapetcu/assignment4/CountingIslandsTest.java
+++ andreeapetcu/assignment4/CountingIslandsTest.java
@@ -9,72 +9,160 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class CountingIslandsTest {
+  
+  CountingIslands countIslandsObject;
+
+  @Before
+  public void setUp() {
+    countIslandsObject = new CountingIslands();
+  }
 
   @Test
   public void testCountIslands_emptyMapPasses() {
     boolean[][] map = {};
-    assertEquals("countingIslands failed", 0, CountingIslands.countingIslands(0, map.length, map));
-    assertEquals("countIslands failed", 0, CountingIslands.countIslands(0, map.length, map));
+    assertEquals("countingIslands failed", 0, countIslandsObject.countingIslands(0, map.length, map));
+    assertEquals("countIslands failed", 0, countIslandsObject.countIslands(0, map.length, map));
   }
 
   @Test
-  public void testCountIslands_validInputPasses1() {
+  public void testCountIslands_randomIslands() {
     boolean[][] map = {{false, true, false, true},
         {true, true, false, false},
         {false, false, true, false},
         {false, false, true, false}};
-    assertEquals("countingIslands failed", 3, CountingIslands.countingIslands(map.length, map[0].length, map));
-    assertEquals("countIslands failed", 3, CountingIslands.countIslands(map.length, map[0].length, map));
+    assertEquals("countingIslands failed", 3, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 3, countIslandsObject.countIslands(map.length, map[0].length, map));
   }
 
   @Test
-  public void testCountIslands_validInputPasses2() {
+  public void testCountIslands_diagonalLand() {
     boolean[][] map = {{false, false, false, true},
         {false, false, true, false},
         {false, true, false, false},
         {true, false, false, false}};
-    assertEquals("countingIslands failed", 4, CountingIslands.countingIslands(map.length, map[0].length, map));
-    assertEquals("countIslands failed", 4, CountingIslands.countIslands(map.length, map[0].length, map));
+    assertEquals("countingIslands failed", 4, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 4, countIslandsObject.countIslands(map.length, map[0].length, map));
   }
 
   @Test
-  public void testCountIslands_validInputPasses3() {
+  public void testCountIslands_noLand() {
     boolean[][] map = {{false, false, false, false},
         {false, false, false, false},
         {false, false, false, false},
         {false, false, false, false}};
-    assertEquals("countingIslands failed", 0, CountingIslands.countingIslands(map.length, map[0].length, map));
-    assertEquals("countIslands failed", 0, CountingIslands.countIslands(map.length, map[0].length, map));
+    assertEquals("countingIslands failed", 0, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 0, countIslandsObject.countIslands(map.length, map[0].length, map));
   }
 
   @Test
-  public void testCountIslands_validInputPasses4() {
+  public void testCountIslands_onlyLand() {
     boolean[][] map = {{true, true, true, true},
         {true, true, true, true},
         {true, true, true, true},
         {true, true, true, true}};
-    assertEquals("countingIslands failed", 1, CountingIslands.countingIslands(map.length, map[0].length, map));
-    assertEquals("countIslands failed", 1, CountingIslands.countIslands(map.length, map[0].length, map));
+    assertEquals("countingIslands failed", 1, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 1, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+  
+  @Test
+  public void testCountIslands_cornerIslands() {
+    boolean[][] map = {{true, false, false, true},
+        {false, false, false, false},
+        {false, false, false, false},
+        {true, false, false, true}};
+    assertEquals("countingIslands failed", 4, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 4, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+
+  @Test
+  public void testCountIslands_leftEdgeIsland() {
+    boolean[][] map = {{true, false, false, false},
+        {true, false, false, false},
+        {true, false, false, false},
+        {true, false, false, false}};
+    assertEquals("countingIslands failed", 1, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 1, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+
+  @Test
+  public void testCountIslands_upperEdgeIsland() {
+    boolean[][] map = {{true, true, true, true},
+        {false, false, false, false},
+        {false, false, false, false},
+        {false, false, false, false}};
+    assertEquals("countingIslands failed", 1, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 1, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+
+  @Test
+  public void testCountIslands_rightEdgeIsland() {
+    boolean[][] map = {{false, false, false, true},
+        {false, false, false, true},
+        {false, false, false, true},
+        {false, false, false, true}};
+    assertEquals("countingIslands failed", 1, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 1, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+
+  @Test
+  public void testCountIslands_lowerEdgeIsland() {
+    boolean[][] map = {{false, false, false, false},
+        {false, false, false, false},
+        {false, false, false, false},
+        {true, true, true, true}};
+    assertEquals("countingIslands failed", 1, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 1, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+
+  @Test
+  public void testCountIslands_lowerAndLeftEdgeIsland() {
+    boolean[][] map = {{true, false, false, false},
+        {true, false, false, false},
+        {true, false, false, false},
+        {true, true, true, true}};
+    assertEquals("countingIslands failed", 1, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 1, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+
+  @Test
+  public void testCountIslands_circularIsland() {
+    boolean[][] map = {{true, true, true, true},
+        {true, false, false, true},
+        {true, false, false, true},
+        {true, true, true, true}};
+    assertEquals("countingIslands failed", 1, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 1, countIslandsObject.countIslands(map.length, map[0].length, map));
+  }
+
+  @Test
+  public void testCountIslands_islandInsideIsland() {
+    boolean[][] map = {{true, true, true, true, true},
+        {true, false, false, false, true},
+        {true, false, true, false, true},
+        {true, false, false, false, true},
+        {true, true, true, true, true}};
+    assertEquals("countingIslands failed", 2, countIslandsObject.countingIslands(map.length, map[0].length, map));
+    assertEquals("countIslands failed", 2, countIslandsObject.countIslands(map.length, map[0].length, map));
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testCountIslands_nullMapFails() {
-    CountingIslands.countingIslands(4, 4, null);
-    CountingIslands.countIslands(4, 4, null);
+    countIslandsObject.countingIslands(4, 4, null);
+    countIslandsObject.countIslands(4, 4, null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testCountIslands_negativeRowsNumberFails() {
     boolean[][] map = {{true, false}, {true, false}};
-    CountingIslands.countingIslands(-1, 4, map);
-    CountingIslands.countIslands(-1, 4, map);
+    countIslandsObject.countingIslands(-1, 4, map);
+    countIslandsObject.countIslands(-1, 4, map);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void testCountIslands_negativeColumnsNumberFails() {
     boolean[][] map = {{true, false}, {true, false}};
-    CountingIslands.countingIslands(4, -1, map);
-    CountingIslands.countIslands(4, -1, map);
+    countIslandsObject.countingIslands(4, -1, map);
+    countIslandsObject.countIslands(4, -1, map);
   }
 
   @Test
@@ -83,7 +171,7 @@ public class CountingIslandsTest {
         {true, true, false, false},
         {false, false, true, false},
         {false, false, true, false}};
-    UnionFind uf = CountingIslands.mergeTilesIntoIslands(map.length, map[0].length, map);
+    UnionFind uf = countIslandsObject.mergeTilesIntoIslands(map.length, map[0].length, map);
     int[] expected = {0, 5, 2, 3, 5, 5, 6, 7, 8, 9, 14, 11, 12, 13, 14, 15};
     assertArrayEquals(expected, uf.getUnions());
   }
@@ -93,7 +181,7 @@ public class CountingIslandsTest {
     boolean[][] map = {{true, true, true},
         {true, true, true},
         {true, true, true}};
-    UnionFind uf = CountingIslands.mergeTilesIntoIslands(map.length, map[0].length, map);
+    UnionFind uf = countIslandsObject.mergeTilesIntoIslands(map.length, map[0].length, map);
     int[] expected = {1, 2, 2, 2, 2, 2, 2, 2, 2};
     assertArrayEquals(expected, uf.getUnions());
   }
@@ -102,7 +190,7 @@ public class CountingIslandsTest {
   public void testDiscoverIsland_emptyMapPasses() {
     ArrayList<CountingIslands.Tile> islandTiles = new ArrayList<>();
     boolean[][] map = {};
-    CountingIslands.discoverIsland(map, new CountingIslands.Tile(0, 0), islandTiles);
+    countIslandsObject.discoverIsland(map, new CountingIslands.Tile(0, 0), islandTiles);
     assertTrue(islandTiles.isEmpty());
   }
 
@@ -114,7 +202,7 @@ public class CountingIslandsTest {
         {false, false, true, false}};
     CountingIslands.Tile currentTile = new CountingIslands.Tile(0, 1);
     ArrayList<CountingIslands.Tile> islandTiles = new ArrayList<>();
-    CountingIslands.discoverIsland(map, currentTile, islandTiles);
+    countIslandsObject.discoverIsland(map, currentTile, islandTiles);
     ArrayList<CountingIslands.Tile> expectedIslandTiles = new ArrayList<>();
     expectedIslandTiles.add(new CountingIslands.Tile(0, 1));
     expectedIslandTiles.add(new CountingIslands.Tile(1, 1));
@@ -130,7 +218,7 @@ public class CountingIslandsTest {
         {false, false, true, false}};
     CountingIslands.Tile currentTile = new CountingIslands.Tile(0, 3);
     ArrayList<CountingIslands.Tile> islandTiles = new ArrayList<>();
-    CountingIslands.discoverIsland(map, currentTile, islandTiles);
+    countIslandsObject.discoverIsland(map, currentTile, islandTiles);
     ArrayList<CountingIslands.Tile> expectedIslandTiles = new ArrayList<>();
     expectedIslandTiles.add(new CountingIslands.Tile(0, 3));
     assertEquals(expectedIslandTiles, islandTiles);
@@ -144,7 +232,7 @@ public class CountingIslandsTest {
         {false, false, true, false}};
     CountingIslands.Tile currentTile = new CountingIslands.Tile(2, 2);
     ArrayList<CountingIslands.Tile> islandTiles = new ArrayList<>();
-    CountingIslands.discoverIsland(map, currentTile, islandTiles);
+    countIslandsObject.discoverIsland(map, currentTile, islandTiles);
     ArrayList<CountingIslands.Tile> expectedIslandTiles = new ArrayList<>();
     expectedIslandTiles.add(new CountingIslands.Tile(2, 2));
     expectedIslandTiles.add(new CountingIslands.Tile(3, 2));
@@ -153,19 +241,19 @@ public class CountingIslandsTest {
 
   @Test(expected = AssertionError.class)
   public void testDiscoverIsland_nullMapFails() {
-    CountingIslands.discoverIsland(null, new CountingIslands.Tile(0, 0), new ArrayList<>());
+    countIslandsObject.discoverIsland(null, new CountingIslands.Tile(0, 0), new ArrayList<>());
   }
 
   @Test(expected = AssertionError.class)
   public void testDiscoverIsland_nullTileFails() {
     boolean[][] map = {{true, false}, {true, false}};
-    CountingIslands.discoverIsland(map, null, new ArrayList<>());
+    countIslandsObject.discoverIsland(map, null, new ArrayList<>());
   }
 
   @Test(expected = AssertionError.class)
   public void testDiscoverIsland_nullAccumulatorFails() {
     boolean[][] map = {{true, false}, {true, false}};
-    CountingIslands.discoverIsland(map, new CountingIslands.Tile(0, 0), null);
+    countIslandsObject.discoverIsland(map, new CountingIslands.Tile(0, 0), null);
   }
 
   @Test
@@ -175,7 +263,7 @@ public class CountingIslandsTest {
         {false, false, true, false},
         {false, false, true, false}};
     CountingIslands.Tile tile = new CountingIslands.Tile(0, 0);
-    ArrayList<CountingIslands.Tile> neighbours = CountingIslands.getNeighbours(map, tile);
+    ArrayList<CountingIslands.Tile> neighbours = countIslandsObject.getNeighbours(map, tile);
     ArrayList<CountingIslands.Tile> expectedNeighbours = new ArrayList<>();
     expectedNeighbours.add(new CountingIslands.Tile(0, 1));
     expectedNeighbours.add(new CountingIslands.Tile(1, 0));
@@ -189,7 +277,7 @@ public class CountingIslandsTest {
         {false, false, true, false},
         {false, false, true, false}};
     CountingIslands.Tile tile = new CountingIslands.Tile(1, 1);
-    ArrayList<CountingIslands.Tile> neighbours = CountingIslands.getNeighbours(map, tile);
+    ArrayList<CountingIslands.Tile> neighbours = countIslandsObject.getNeighbours(map, tile);
     ArrayList<CountingIslands.Tile> expectedNeighbours = new ArrayList<>();
     expectedNeighbours.add(new CountingIslands.Tile(0, 1));
     expectedNeighbours.add(new CountingIslands.Tile(1, 0));
@@ -200,13 +288,13 @@ public class CountingIslandsTest {
 
   @Test(expected = AssertionError.class)
   public void testGetNeighbours_nullMapFails() {
-    CountingIslands.getNeighbours(null, new CountingIslands.Tile(0, 0));
+    countIslandsObject.getNeighbours(null, new CountingIslands.Tile(0, 0));
   }
 
   @Test
   public void testGetNeighbours_emptyMapPasses() {
     boolean[][] map = {};
-    assertTrue(CountingIslands.getNeighbours(map, new CountingIslands.Tile(0, 0)).isEmpty());
+    assertTrue(countIslandsObject.getNeighbours(map, new CountingIslands.Tile(0, 0)).isEmpty());
   }
 
   @Test(expected = AssertionError.class)
@@ -215,7 +303,7 @@ public class CountingIslandsTest {
         {true, true, false, false},
         {false, false, true, false},
         {false, false, true, false}};
-    CountingIslands.getNeighbours(map, null);
+    countIslandsObject.getNeighbours(map, null);
   }
 
   @Test(expected = AssertionError.class)
@@ -225,43 +313,43 @@ public class CountingIslandsTest {
         {false, false, true, false},
         {false, false, true, false}};
     CountingIslands.Tile tile = new CountingIslands.Tile(5, 5);
-    CountingIslands.getNeighbours(map, tile);
+    countIslandsObject.getNeighbours(map, tile);
   }
 
   @Test
   public void testIsValidTile_validCornerTilePasses1() {
-    assertTrue(CountingIslands.isValidTile(new CountingIslands.Tile(0, 0), 4, 4));
+    assertTrue(countIslandsObject.isValidTile(new CountingIslands.Tile(0, 0), 4, 4));
   }
 
   @Test
   public void testIsValidTile_validCornerTilePasses2() {
-    assertTrue(CountingIslands.isValidTile(new CountingIslands.Tile(3, 3), 4, 4));
+    assertTrue(countIslandsObject.isValidTile(new CountingIslands.Tile(3, 3), 4, 4));
   }
 
   @Test
   public void testIsValidTile_validMiddleTilePasses2() {
-    assertTrue(CountingIslands.isValidTile(new CountingIslands.Tile(2, 2), 4, 4));
+    assertTrue(countIslandsObject.isValidTile(new CountingIslands.Tile(2, 2), 4, 4));
   }
 
 
   @Test(expected = AssertionError.class)
   public void testIsValidTile_nullTileFails() {
-    CountingIslands.isValidTile(null, 4, 4);
+    countIslandsObject.isValidTile(null, 4, 4);
   }
 
   @Test
   public void testIsValidTile_outOfBoundsTileFails1() {
-    assertFalse(CountingIslands.isValidTile(new CountingIslands.Tile(4, 4), 4, 4));
+    assertFalse(countIslandsObject.isValidTile(new CountingIslands.Tile(4, 4), 4, 4));
   }
 
   @Test
   public void testIsValidTile_outOfBoundsTileFails2() {
-    assertFalse(CountingIslands.isValidTile(new CountingIslands.Tile(-1, 4), 4, 4));
+    assertFalse(countIslandsObject.isValidTile(new CountingIslands.Tile(-1, 4), 4, 4));
   }
 
   @Test(expected = AssertionError.class)
   public void testIsValidTile_zeroMaxRowFails() {
-    assertFalse(CountingIslands.isValidTile(new CountingIslands.Tile(3, 3), 0, 4));
+    assertFalse(countIslandsObject.isValidTile(new CountingIslands.Tile(3, 3), 0, 4));
   }
 
   @Test(expected = AssertionError.class)
