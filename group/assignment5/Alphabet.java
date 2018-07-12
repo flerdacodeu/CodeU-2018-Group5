c110d33f936b0509bd48479ec04684a5eefc9e1b
Sophie Kaleba <sophie.kaleba@gmail.com>
2018-07-12 15:33:25 +0200

    almost there. miss the sort method, and documentation

--- group/assignment5/Alphabet.java
+++ group/assignment5/Alphabet.java
@@ -1,58 +1,75 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Alphabet {
 
 	public String[] dictionary;
-	public List<Character> unknownAlphabet;
+	public Graph unknownAlphabet;
 	
 	public Alphabet(String[] dictionary) {
 		this.dictionary = dictionary;
-		this.unknownAlphabet = new ArrayList<Character>();
+		this.unknownAlphabet = new Graph();
 	}
 	
 	public List<Character> build() {
 		int dictSize = this.dictionary.length;
 		
 		for (int i = 0; i < dictSize-1 ; i++) {
-			char[] auxOrder = this.compareWords(this.dictionary[i], this.dictionary[i+1]);
-			this.insertOrder(auxOrder);
+			this.compareWords(this.dictionary[i], this.dictionary[i+1]);
 		}
-		return this.unknownAlphabet;
+		return this.sort(this.unknownAlphabet);
 	}
 	
-	public void insertOrder(char[] order) {
+	public List<Character> sort(Graph unsortedAlphabet) {
 		
 	}
 	
-	public char[] compareWords(String word1, String word2) {
+
+	
+	public void compareWords(String word1, String word2) {
 		if (word1.isEmpty() && word2.isEmpty()) {
 			throw new IllegalArgumentException("one of the words is empty");
 		}
 		
-		int i = 0;
-		boolean inBounds = true;
 		int sizeWord1 = word1.length();
 		int sizeWord2 = word2.length();
-		char charWord1 = word1.charAt(i);
-		char charWord2 = word2.charAt(i);
+		boolean firstDifference = true;
+		int longestWord;
+		int i = 0;
 		
-		while ( (charWord1 == charWord2) && inBounds ) {
-			i++;
-			charWord1 = word1.charAt(i);
-			charWord2 = word2.charAt(i);
-			if (i > sizeWord1 && i > sizeWord2) {
-				i--;
-				charWord1 = word1.charAt(i);
-				charWord2 = word2.charAt(i);
-				inBounds = false;	
-			}
-		}	
+		if (sizeWord1 > sizeWord2) { longestWord = 1;}
+		else {longestWord = 2;}
 		
-		int comparison = charWord1 - charWord2;
-		if (comparison > 0) return new char[]{charWord1, charWord2};
-		else if (comparison == 0) return new char[]{charWord1};
-		else return new char[]{charWord2, charWord1};
+		for (; i < Math.min(sizeWord1, sizeWord2); i++) {
+			char charWord1 = word1.charAt(i);
+			char charWord2 = word2.charAt(i);
+			this.compareChar(charWord1, charWord2, firstDifference);
+		}
+		
+		// the last letters of the longest word has to be part of the alphabet
+		if (longestWord == 1) this.browseLastLettersOfLongest(word1, sizeWord1, i);
+		else if (longestWord == 2) this.browseLastLettersOfLongest(word2, sizeWord2, i);
+		
+		
+	
 	}
+	
+	public void compareChar(char c1, char c2, boolean firstDifference) {
+		this.unknownAlphabet.addVertex(c1);
+		this.unknownAlphabet.addVertex(c2);
+		
+		if (firstDifference && (c1 != c2)) {
+			this.unknownAlphabet.updateEdge(c1, c2);
+			firstDifference = false;
+		}			
+	}
+	
+	
+	public void browseLastLettersOfLongest(String word, int wordSize, int i) {
+			for (; i<wordSize; i++) {
+				this.unknownAlphabet.addVertex(word.charAt(i));
+			}
+	}
+	
+	
 	
 }
