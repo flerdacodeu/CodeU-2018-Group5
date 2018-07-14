8e86620894e54c2a3110da96fc53d59e9ecf2301
Andreea Petcu <38542030+andreeapetcu@users.noreply.github.com>
2018-07-14 01:33:27 +0100

    basic tests (more to be added)

---
+++ group/assignment5/UnknownAlphabetTest.java
@@ -0,0 +1,101 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class UnknownAlphabetTest {
+
+  @Test
+  public void testExtractAlphabet_emptyDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    List<Character> alphabet = unknownAlphabet.extractAlphabet(new ArrayList<>());
+    assertTrue(alphabet.isEmpty());
+  }
+
+  @Test
+  public void testExtractAlphabet_oneLetterWords() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    List<Character> alphabet = unknownAlphabet.extractAlphabet(Arrays.asList("q","g","m"));
+    List<Character> expectedAlphabet = Arrays.asList('q', 'g', 'm');
+    assertEquals(expectedAlphabet, alphabet);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testExtractAlphabet_nullDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    unknownAlphabet.extractAlphabet(null);
+  }
+
+  @Test
+  public void testExtractAlphabet_validDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    List<Character> alphabet = unknownAlphabet.extractAlphabet(Arrays.asList("art", "rat", "cat", "car"));
+    List<Character> expectedAlphabet = Arrays.asList('a', 't', 'r', 'c');
+    assertEquals(expectedAlphabet, alphabet);
+  }
+
+  @Test
+  public void testExtractAllAlphabets_emptyDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    List<List<Character>> alphabets = unknownAlphabet.extractAllAlphabets(new ArrayList<>());
+    assertTrue(alphabets.isEmpty());
+  }
+
+  @Test
+  public void testExtractAllAlphabets_oneLetterWords() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    List<List<Character>> alphabets = unknownAlphabet.extractAllAlphabets(Arrays.asList("a","b","c"));
+    List<List<Character>> expectedAlphabets = Arrays.asList(Arrays.asList('a', 'b', 'c'));
+    assertEquals(expectedAlphabets, alphabets);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testExtractAllAlphabets_nullDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    unknownAlphabet.extractAllAlphabets(null);
+  }
+
+  @Test
+  public void testExtractAllAlphabets_validDictionary() {
+    UnknownAlphabet a = new UnknownAlphabet();
+    List<List<Character>> alphabets = a.extractAllAlphabets(Arrays.asList("art", "rat", "cat", "car"));
+    List<Character> expectedOrder1 = Arrays.asList('a', 't', 'r', 'c');
+    List<Character> expectedOrder2 = Arrays.asList('t', 'a', 'r', 'c');
+    List<List<Character>> expectedOrderLists = new ArrayList<>();
+    expectedOrderLists.add(expectedOrder1);
+    expectedOrderLists.add(expectedOrder2);
+    assertEquals(expectedOrderLists, alphabets);
+  }
+
+   @Test
+  public void testIsConsistentDictionary_emptyDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    assertTrue(unknownAlphabet.isConsistentDictionary(new ArrayList<>()));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIsConsistentDictionary_nullDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    assertTrue(unknownAlphabet.isConsistentDictionary(null));
+  }
+
+  @Test
+  public void testIsConsistentDictionary_inconsistentDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    List<String> dictionary = Arrays.asList("cat", "talk", "tattoo", "lasagna");
+    assertFalse(unknownAlphabet.isConsistentDictionary(dictionary));
+  }
+
+  @Test
+  public void testIsConsistentDictionary_consistentDictionary() {
+    UnknownAlphabet unknownAlphabet = new UnknownAlphabet();
+    List<String> dictionary = Arrays.asList("cat", "car", "tattoo", "alpaca");
+    assertTrue(unknownAlphabet.isConsistentDictionary(dictionary));
+  }
+}
