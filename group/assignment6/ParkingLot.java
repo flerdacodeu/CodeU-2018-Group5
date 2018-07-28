527b81012a25520a0f7277180823eae81d7dd829
Andreea Petcu <38542030+andreeapetcu@users.noreply.github.com>
2018-07-28 15:05:19 +0100

    Create ParkingLot.java

---
+++ group/assignment6/ParkingLot.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ParkingLot {
+  public List<Move> rearrangeCars(State startState, State endState) {
+    //TODO: implement algorithm
+    List<Move> sequenceOfMoves = new ArrayList<>();
+    return sequenceOfMoves;
+  }
+}
+  
