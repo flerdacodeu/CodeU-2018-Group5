e852b1cb86f83636090b14da7cb98fa68a77d58b
Andreea Petcu <38542030+andreeapetcu@users.noreply.github.com>
2018-07-28 15:01:09 +0100

    Create Move.java

---
+++ group/assignment6/Move.java
@@ -0,0 +1,36 @@
+public class Move {
+  private final Car car;
+  private final ParkingSpace fromSpace;
+  private final ParkingSpace toSpace;
+
+  public Move(Car car, ParkingSpace fromSpace, ParkingSpace toSpace) {
+    this.car = car;
+    this.fromSpace = fromSpace;
+    this.toSpace = toSpace;
+  }
+
+  public void getMoveInfo() {
+    String info = "move car #" + car.getIdentifier() +
+        " from space " + fromSpace.getIdentifier() + " to space " + toSpace.getIdentifier();
+    System.out.println(info);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null || this.getClass() != obj.getClass()) {
+      return false;
+    }
+
+    Move move = (Move) obj;
+    return this.car.equals(move.car) && this.fromSpace.equals(move.fromSpace) && this.toSpace.equals(move.toSpace);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = 17;
+    result = 31 * result + car.hashCode();
+    result = 31 * result + fromSpace.hashCode();
+    result = 31 * result + toSpace.hashCode();
+    return result;
+  }
+}
