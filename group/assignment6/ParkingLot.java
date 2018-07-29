ec7081c004bd7a1768d627384a5e5e518bd2e802
Andreea Petcu <38542030+andreeapetcu@users.noreply.github.com>
2018-07-29 23:51:25 +0100

    added a complete and working method for rearranging cars
    - the idea is similar to the one used current method rearrangeCars2

--- group/assignment6/ParkingLot.java
+++ group/assignment6/ParkingLot.java
@@ -1,7 +1,92 @@
-package template;
 import java.util.*;
 
 public class ParkingLot {
+  	
+  private static final Car NO_CAR = null;	
+	
+  /**
+   * Generates a sequence of moves needed to rearrange the cars from a given start state into
+   * a given end state.
+   *
+   * @param startState the initial state of the parking space
+   * @param endState the final state of the parking space
+   * @return the sequence of moves that rearrange the cars
+   * @throws IllegalArgumentException if the start state or the end state are null
+   */
+  public List<Move> rearrangeCars(State startState, State endState) {
+    //TODO: more input validation
+    if (startState == null || endState == null) {
+      throw new IllegalArgumentException();
+    }
+
+    List<Move> sequenceOfMoves = new ArrayList<>();
+    rearrangeCars(startState, endState, sequenceOfMoves);
+    return sequenceOfMoves;
+  }
+
+  /**
+   * Helper method for generating the sequence of moves needed to rearrange the cars.
+   *
+   * Algorithm:
+   *  1. compare the position of the empty parking space in the current state with the position of
+   *  the empty parking space in the final state
+   *  2. if they are the same then find in the current state the first car that is not parked in the
+   *  right parking space and park it in the empty space
+   *  3. while the positions are different, get the car from the final state that needs to be
+   *  parked in the position of empty parking space in the current state and park it there
+   *  4. repeat previous steps
+   *
+   * @param currentState the current state of the parking space
+   * @param endState the final state of the parking space
+   * @param sequenceOfMoves the sequence of moves that rearrange the cars
+   */
+  private void rearrangeCars(State currentState, State endState, List<Move> sequenceOfMoves) {
+    ParkingSpace emptySpaceCurrentState = currentState.getEmptyParkingSpace();
+    ParkingSpace emptySpaceEndState = endState.getEmptyParkingSpace();
+
+    if (emptySpaceCurrentState.equals(emptySpaceEndState)) {
+      Car carInWrongSpace = getFirstCarInWrongSpace(currentState, endState);
+      // if NO_CAR is returned then all the cars are in the right parking space
+      if (carInWrongSpace == NO_CAR) {
+        return;
+      }
+      currentState.parkCarInSpace(carInWrongSpace, emptySpaceCurrentState);
+      ParkingSpace carSpace = currentState.getParkingSpaceOfCar(carInWrongSpace);
+      sequenceOfMoves.add(new Move(carInWrongSpace, carSpace, emptySpaceCurrentState));
+    }
+    while (!emptySpaceCurrentState.equals(emptySpaceEndState)) {
+      Car expectedCar = endState.getCarParkedInSpace(emptySpaceCurrentState);
+      ParkingSpace positionOfExpectedCar = currentState.getParkingSpaceOfCar(expectedCar);
+      currentState.parkCarInSpace(expectedCar, emptySpaceCurrentState);
+      sequenceOfMoves.add(new Move(expectedCar, positionOfExpectedCar, emptySpaceCurrentState));
+      emptySpaceCurrentState = currentState.getEmptyParkingSpace();
+      emptySpaceEndState = endState.getEmptyParkingSpace();
+    }
+    rearrangeCars(currentState, endState, sequenceOfMoves);
+  }
+
+  /**
+   * Finds the first car in the given current state of the parking lot that is not parked in the space
+   * it must be in the final state of the parking lot
+   *
+   * @param currentState the current state of the parking lot
+   * @param endState the final state of the parking lot
+   * @return the first car that is parked in the wrong place, or null if no such car is found
+   */
+  private Car getFirstCarInWrongSpace(State currentState, State endState) {
+    for (ParkingSpace space : currentState.getAllParkingSpaces()) {
+      Car currentCar = currentState.getCarParkedInSpace(space);
+      Car expectedCar = endState.getCarParkedInSpace(space);
+      if(currentCar != NO_CAR && !currentCar.equals(expectedCar)) {
+        return currentCar;
+      }
+    }
+    // all the cars are in the right parking space
+    return NO_CAR;
+  }
+	
+	
+	
 	/*
 	 * Iteration algorithm, we go through all slots and rearrange cars by emptying
 	 * destination slot and moving car there
