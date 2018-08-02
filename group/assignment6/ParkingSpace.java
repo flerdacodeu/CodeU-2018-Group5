015490e22693661feeacdf831f9d7c29f8d8e506
Andreea Petcu <38542030+andreeapetcu@users.noreply.github.com>
2018-08-02 18:47:00 +0100

    added Set<Car> reservations
    - added additional constructor to allow creation of a space with reservations
    - added methods getReservations(), addReservations(), reserveSpace(), removeReservation()
    - modified equals() and hashCode()

--- group/assignment6/ParkingSpace.java
+++ group/assignment6/ParkingSpace.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Assumptions:
  * - the unique identifier of a parking space is a non-zero positive integer
@@ -6,14 +9,48 @@
 public class ParkingSpace {
 
   private final int identifier;
+  // set of the cars that can be parked on this space
+  private Set<Car> reservations;
 
   public ParkingSpace(int identifier) {
     assert identifier > 0;
     this.identifier = identifier;
+      reservations = new HashSet<>();
+  }
+
+  public ParkingSpace(int identifier, Set<Car> reservations) {
+    assert identifier > 0 && reservations != null;
+    this.identifier = identifier;
+    this.reservations = reservations;
   }
 
   public int getIdentifier() {
     return identifier;
+  }
+  
+  public Set<Car> getReservations() {
+    return reservations;
+  }
+
+  public void addReservations(Set<Car> reservations) {
+    if (reservations == null) {
+      throw new IllegalArgumentException();
+    }
+    this.reservations.addAll(reservations);
+  }
+
+  public void reserveSpace(Car car) {
+    if (car == null) {
+      throw new IllegalArgumentException();
+    }
+    reservations.add(car);
+  }
+
+  public void removeReservation(Car car) {
+    if (car == null || !reservations.contains(car)) {
+      throw new IllegalArgumentException();
+    }
+    reservations.remove(car);
   }
 
   @Override
@@ -23,13 +60,23 @@ public class ParkingSpace {
     }
 
     ParkingSpace parkingSpace = (ParkingSpace) obj;
-    return this.identifier == parkingSpace.identifier;
+    
+    boolean equal = this.identifier == parkingSpace.identifier;
+    if (reservations != null) {
+      equal = equal && reservations.equals(parkingSpace.getReservations());
+    }
+    return equal;
   }
 
   @Override
   public int hashCode() {
     int result = 17;
     result = 31 * result + identifier;
+    if (!reservations.isEmpty()) {
+      for (Car car : reservations) {
+        result = 31 * result + car.hashCode();
+      }
+    }
     return result;
   }
 }
