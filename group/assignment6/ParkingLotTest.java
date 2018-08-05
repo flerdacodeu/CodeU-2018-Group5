231e0673c0dedcf366c57c52a07b93229eff7aac
Sophie Kaleba <sophie.kaleba@gmail.com>
2018-08-05 21:05:21 +0200

    for later - comment one test

--- group/assignment6/ParkingLotTest.java
+++ group/assignment6/ParkingLotTest.java
@@ -27,106 +27,106 @@ public class ParkingLotTest {
 	}
 	
 
-//	@Test
-//	public void testRearrangeCars_emptyParkingSlot() {
-//		//TODO-should raise an exception if no car in the parking slot?
-//		State startState = new State(Arrays.asList(-1,-1,-1,-1));
-//		State endState = new State(Arrays.asList(-1,-1,-1,-1));
-//		ParkingLot lot = new ParkingLot();
-//		List<Move> actualResult = lot.rearrangeCars(startState, endState);
-//		List<Move> expectedResult = new ArrayList<Move>(); 
-//		assertEquals(expectedResult, actualResult);
-//	}
-//	
-//	
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testRearrangeCars_nullStartState() {
-//		State startState = null;
-//		State endState = new State(Arrays.asList(1,2,-1,3));
-//		ParkingLot lot = new ParkingLot();
-//		lot.rearrangeCars(startState, endState);
-//	}
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testRearrangeCars_nullEndState() {
-//		State startState = new State(Arrays.asList(1,2,-1,3));
-//		State endState = null;
-//		ParkingLot lot = new ParkingLot();
-//		lot.rearrangeCars(startState, endState);
-//	}
-//	
-//	@Test
-//	public void testRearrangeCars_commonCase() {
-//		State startState = new State(Arrays.asList(1,2,-1,3));
-//		State endState = new State(Arrays.asList(3,1,2,-1));
-//		ParkingLot lot = new ParkingLot();
-//		List<Move> actualResult = lot.rearrangeCars(startState, endState);
-//		
-//		//expected result set-up
-//		List<Move> expectedResult = new ArrayList<Move>(); 
-//		List<Car> cars = this.generateXcars(3);
-//		List<ParkingSpace> spaces = this.generateXspaces(4);
-//		Move move1 = new Move(cars.get(1), spaces.get(1), spaces.get(2));
-//		Move move2 = new Move(cars.get(0), spaces.get(0), spaces.get(1));
-//		Move move3 = new Move(cars.get(2), spaces.get(3), spaces.get(0));
-//		expectedResult.add(move1);
-//		expectedResult.add(move2);
-//		expectedResult.add(move3);
-//		
-//		assertEquals(expectedResult, actualResult);
-//	}
-//	
-//	@Test
-//	public void testRearrangeCars_sameEmptySpot() {
-//		State startState = new State(Arrays.asList(1,2,-1,3));
-//		State endState = new State(Arrays.asList(3,1,-1,2));
-//		ParkingLot lot = new ParkingLot();
-//		List<Move> actualResult = lot.rearrangeCars(startState, endState);
-//		
-//		//expected result set-up
-//		List<Move> expectedResult = new ArrayList<Move>(); 
-//		List<Car> cars = this.generateXcars(3);
-//		List<ParkingSpace> spaces = this.generateXspaces(4);
-//		Move move1 = new Move(cars.get(0), spaces.get(0), spaces.get(2));
-//		Move move2 = new Move(cars.get(2), spaces.get(3), spaces.get(0));
-//		Move move3 = new Move(cars.get(1), spaces.get(1), spaces.get(3));
-//		Move move4 = new Move(cars.get(0), spaces.get(2), spaces.get(1));
-//		expectedResult.add(move1);
-//		expectedResult.add(move2);
-//		expectedResult.add(move3);
-//		expectedResult.add(move4);
-//		
-//		assertEquals(expectedResult, actualResult);
-//	}
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testRearrangeCars_moreThanOneEmptySpace() {
-//		//TODO - should definitely raise an exception
-//		State startState = new State(Arrays.asList(1,2,-1,3,-1));
-//		State endState = new State(Arrays.asList(3,1,-1,2,-1));
-//		ParkingLot lot = new ParkingLot();
-//		lot.rearrangeCars(startState, endState);
-//	}
-//	
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testRearrangeCars_inconsistentNumberOfSpacesAndCars() {
-//		//TODO - should definitely raise an exception
-//		State startState = new State(Arrays.asList(1,2,-1,3,4));
-//		State endState = new State(Arrays.asList(3,1,-1,2));
-//		ParkingLot lot = new ParkingLot();
-//		lot.rearrangeCars(startState, endState);
-//	}
-//	
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testRearrangeCars_inconsistentCarIdentifiers() {
-//		//TODO - should definitely raise an exception
-//		State startState = new State(Arrays.asList(1,2,-1,3));
-//		State endState = new State(Arrays.asList(3,1,4,-1));
-//		ParkingLot lot = new ParkingLot();
-//		lot.rearrangeCars(startState, endState);
-//	}
+	@Test
+	public void testRearrangeCars_emptyParkingSlot() {
+		//TODO-should raise an exception if no car in the parking slot?
+		State startState = new State(Arrays.asList(-1,-1,-1,-1));
+		State endState = new State(Arrays.asList(-1,-1,-1,-1));
+		ParkingLot lot = new ParkingLot();
+		List<Move> actualResult = lot.rearrangeCars(startState, endState);
+		List<Move> expectedResult = new ArrayList<Move>(); 
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testRearrangeCars_nullStartState() {
+		State startState = null;
+		State endState = new State(Arrays.asList(1,2,-1,3));
+		ParkingLot lot = new ParkingLot();
+		lot.rearrangeCars(startState, endState);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testRearrangeCars_nullEndState() {
+		State startState = new State(Arrays.asList(1,2,-1,3));
+		State endState = null;
+		ParkingLot lot = new ParkingLot();
+		lot.rearrangeCars(startState, endState);
+	}
+	
+	@Test
+	public void testRearrangeCars_commonCase() {
+		State startState = new State(Arrays.asList(1,2,-1,3));
+		State endState = new State(Arrays.asList(3,1,2,-1));
+		ParkingLot lot = new ParkingLot();
+		List<Move> actualResult = lot.rearrangeCars(startState, endState);
+		
+		//expected result set-up
+		List<Move> expectedResult = new ArrayList<Move>(); 
+		List<Car> cars = this.generateXcars(3);
+		List<ParkingSpace> spaces = this.generateXspaces(4);
+		Move move1 = new Move(cars.get(1), spaces.get(1), spaces.get(2));
+		Move move2 = new Move(cars.get(0), spaces.get(0), spaces.get(1));
+		Move move3 = new Move(cars.get(2), spaces.get(3), spaces.get(0));
+		expectedResult.add(move1);
+		expectedResult.add(move2);
+		expectedResult.add(move3);
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test
+	public void testRearrangeCars_sameEmptySpot() {
+		State startState = new State(Arrays.asList(1,2,-1,3));
+		State endState = new State(Arrays.asList(3,1,-1,2));
+		ParkingLot lot = new ParkingLot();
+		List<Move> actualResult = lot.rearrangeCars(startState, endState);
+		
+		//expected result set-up
+		List<Move> expectedResult = new ArrayList<Move>(); 
+		List<Car> cars = this.generateXcars(3);
+		List<ParkingSpace> spaces = this.generateXspaces(4);
+		Move move1 = new Move(cars.get(0), spaces.get(0), spaces.get(2));
+		Move move2 = new Move(cars.get(2), spaces.get(3), spaces.get(0));
+		Move move3 = new Move(cars.get(1), spaces.get(1), spaces.get(3));
+		Move move4 = new Move(cars.get(0), spaces.get(2), spaces.get(1));
+		expectedResult.add(move1);
+		expectedResult.add(move2);
+		expectedResult.add(move3);
+		expectedResult.add(move4);
+		
+		assertEquals(expectedResult, actualResult);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testRearrangeCars_moreThanOneEmptySpace() {
+		//TODO - should definitely raise an exception
+		State startState = new State(Arrays.asList(1,2,-1,3,-1));
+		State endState = new State(Arrays.asList(3,1,-1,2,-1));
+		ParkingLot lot = new ParkingLot();
+		lot.rearrangeCars(startState, endState);
+	}
+	
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testRearrangeCars_inconsistentNumberOfSpacesAndCars() {
+		//TODO - should definitely raise an exception
+		State startState = new State(Arrays.asList(1,2,-1,3,4));
+		State endState = new State(Arrays.asList(3,1,-1,2));
+		ParkingLot lot = new ParkingLot();
+		lot.rearrangeCars(startState, endState);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testRearrangeCars_inconsistentCarIdentifiers() {
+		//TODO - should definitely raise an exception
+		State startState = new State(Arrays.asList(1,2,-1,3));
+		State endState = new State(Arrays.asList(3,1,4,-1));
+		ParkingLot lot = new ParkingLot();
+		lot.rearrangeCars(startState, endState);
+	}
 	
 	@Test
 	public void testComputeAllPossibleSequences_commonCase() {
@@ -162,38 +162,38 @@ public class ParkingLotTest {
 		assertEquals(expectedResult, actualResult);
 	}
 	
-	@Test
-	public void testComputeAllPossibleSequences_commonCase2() {
-		State startState = new State(Arrays.asList(1,2,-1));
-		State endState = new State(Arrays.asList(2,1,-1));
-		ParkingLot lot = new ParkingLot();
-		List<List<Move>> actualResult = lot.computeAllPossibleSequences(startState, endState);
-		
-		//expected result set-up
-		List<List<Move>> expectedResult = new ArrayList<List<Move>>(); 
-		List<Move> firstSequence = new ArrayList<Move>();
-		List<Move> secondSequence = new ArrayList<Move>();
-		List<Car> cars = this.generateXcars(2);
-		List<ParkingSpace> spaces = this.generateXspaces(3);
-		
-		Move move1_1 = new Move(cars.get(0), spaces.get(0), spaces.get(2));
-		Move move2_1 = new Move(cars.get(1), spaces.get(1), spaces.get(0));
-		Move move3_1 = new Move(cars.get(0), spaces.get(2), spaces.get(1));
-		firstSequence.add(move1_1);
-		firstSequence.add(move2_1);
-		firstSequence.add(move3_1);
-		
-		Move move1_2 = new Move(cars.get(1), spaces.get(1), spaces.get(2));
-		Move move2_2 = new Move(cars.get(0), spaces.get(0), spaces.get(1));
-		Move move3_2 = new Move(cars.get(1), spaces.get(2), spaces.get(0));
-		secondSequence.add(move1_2);
-		secondSequence.add(move2_2);
-		secondSequence.add(move3_2);
-		
-		expectedResult.add(firstSequence);
-		expectedResult.add(secondSequence);
-		
-		assertEquals(expectedResult, actualResult);
-	}
+//	@Test
+//	public void testComputeAllPossibleSequences_commonCase2() {
+//		State startState = new State(Arrays.asList(1,2,-1));
+//		State endState = new State(Arrays.asList(2,1,-1));
+//		ParkingLot lot = new ParkingLot();
+//		List<List<Move>> actualResult = lot.computeAllPossibleSequences(startState, endState);
+//		
+//		//expected result set-up
+//		List<List<Move>> expectedResult = new ArrayList<List<Move>>(); 
+//		List<Move> firstSequence = new ArrayList<Move>();
+//		List<Move> secondSequence = new ArrayList<Move>();
+//		List<Car> cars = this.generateXcars(2);
+//		List<ParkingSpace> spaces = this.generateXspaces(3);
+//		
+//		Move move1_1 = new Move(cars.get(0), spaces.get(0), spaces.get(2));
+//		Move move2_1 = new Move(cars.get(1), spaces.get(1), spaces.get(0));
+//		Move move3_1 = new Move(cars.get(0), spaces.get(2), spaces.get(1));
+//		firstSequence.add(move1_1);
+//		firstSequence.add(move2_1);
+//		firstSequence.add(move3_1);
+//		
+//		Move move1_2 = new Move(cars.get(1), spaces.get(1), spaces.get(2));
+//		Move move2_2 = new Move(cars.get(0), spaces.get(0), spaces.get(1));
+//		Move move3_2 = new Move(cars.get(1), spaces.get(2), spaces.get(0));
+//		secondSequence.add(move1_2);
+//		secondSequence.add(move2_2);
+//		secondSequence.add(move3_2);
+//		
+//		expectedResult.add(firstSequence);
+//		expectedResult.add(secondSequence);
+//		
+//		assertEquals(expectedResult, actualResult);
+//	}
 
 }
